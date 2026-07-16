6df2cb0c289f6e8e9903ba168eed56802ada3c2b
Rintu <161097725+softgineer@users.noreply.github.com>
2026-07-17 00:32:02 +0100

    Add viewport meta tag to index.js
    Added viewport meta tag for responsive design.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><App /></React.StrictMode>);
